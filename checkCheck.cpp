f08eb73ad5d7207157c96ac0b79b537299480b2d
Tristan <tris1313@me.com>
2019-09-18 00:20:57 +0930

    Added checkCheck function

---
+++ checkCheck.cpp
@@ -0,0 +1,11 @@
+/*  This function takes a given position of the board, and a
+    boolean isWhiteTurn, and checks if the position has resulted
+    in the previous moving player being in check.
+*/
+
+bool checkCheck(Board position, bool isWhiteTurn) {
+
+  /* Check if the non-moving player is in check */
+
+  return 1;
+}
