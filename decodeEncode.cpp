af09594418a4dd5eb4969c32aea9e7008d1b5264
James Edwards <james.n.edwards@student.adelaide.edu.au>
2019-09-18 13:44:29 +0930

    Started decodeEncode

--- decodeEncode.cpp
+++ decodeEncode.cpp
@@ -5,12 +5,19 @@ using std::cout;
 using std::cin;
 using std::string;
 
+//Takes input from cin and encodes to number from 0-63.
 int inputFunc(void) {
-  /* code */
+  string input;
+  cin >> input;
+  switch (input) {
+    case :
+  }
   return 0;
 }
 
 string encode(int code){
-  /* code */
+
+
+
   return "";
 }
