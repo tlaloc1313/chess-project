af6914351db4a49fb3a005adbf602b21ce3b3e26
tlaloc1313 <tris1313@me.com>
2019-09-18 18:00:14 +0930

    bug fix

--- pieceFiles/Pieces.h
+++ pieceFiles/Pieces.h
@@ -16,8 +16,8 @@ public:
   virtual void move(int square);
   int beTaken();
   int getPosition();
-  int getType();
-  int getIsWhite();
+  char getType();
+  bool getIsWhite();
   virtual ~Piece ();
 };
 
